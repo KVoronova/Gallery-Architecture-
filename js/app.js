88b7c8dffb1c5a7d7da06c9f29c17c403ebb463d
KVoronova <voronova.vs@gmail.com>
2025-02-28 17:15:03 +0300

    update gallery

--- js/app.js
+++ js/app.js
@@ -4,9 +4,18 @@ setTimeout(function() {
 
     document.body.classList.add('loaded')
 
+    if(window.matchMedia('(min-width:992px)').matches) {
+
     Draggable.create('.gallery', {
         bounds: 'body',
         inertia: true
         });
-    });
+
+       }
+
+    }, 200);
 }
+
+
+
+
